b8eb4c77a2b34a1cb43a2f04262c0f258a026f2f
Prateik Lohani <lohaniprateik@gmail.com>
2026-01-26 10:27:45 +0530

    add: drag n drop provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { ClipsContextProvider } from './context/ClipsContextProvider.tsx'
 import { NotificationContextProvider } from './context/NotificationContextProvider.tsx'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ClipsContextProvider>
 			<NotificationContextProvider>
-				<App />
+				<DndProvider backend={HTML5Backend}>
+					<App />
+				</DndProvider>
 			</NotificationContextProvider>
 		</ClipsContextProvider>
 	</StrictMode>,
